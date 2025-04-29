76b2ca3f045d73c1c364ae2bb311dc843e9c631e
Dawid Brach <154083605+DawidBrrr@users.noreply.github.com>
2025-04-29 14:48:48 +0200

    Delete FutureExample.java

--- FutureExample.java
+++
@@ -1,9 +0,0 @@
-package com.casino.future;
-
-public class FutureExample {
-    public static void main(String[] args) {
-        TaskManager taskManager = new TaskManager();
-        MenuService menuService = new MenuService(taskManager);
-        menuService.start();
-    }
-}
